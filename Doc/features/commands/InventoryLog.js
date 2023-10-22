7d7ed63f07ea10cde4d4cb449534258634e5e4fb
Dalwyn <100111505+DalwynWasTaken@users.noreply.github.com>
2023-10-22 23:46:17 +0200

    Update InventoryLog.js
    Made it work, somewhat.
    
    There is some issues with enchants and the lore but it has been mostly mitigated for Skyblock's use

--- Doc/features/commands/InventoryLog.js
+++ Doc/features/commands/InventoryLog.js
@@ -1,67 +1,94 @@
-import { ChestMenu } from "../../../ChestMenu/index"
-import { PREFIX, chat, getJsonDataFromFile, saveToFile } from "../../utils/Utils"
+import { ChestMenu } from "../../../ChestMenu/index";
+import { PREFIX, getJsonDataFromFile, saveToFile } from "../../utils/Utils";
 
-const inventoryLogs = getJsonDataFromFile("data/InventoryLogs.json", [])
+const inventoryLogs = getJsonDataFromFile("data/InventoryLogs.json", {});
 
-const makeInvChest = (invName, rows = 4) => new ChestMenu(`&b&lInventory&f: &6&l${invName}`, rows)
-const makeInvObj = () => {
-    const res = {}
+function logCurrentInventory() {
+    const inventory = {};
 
-    Player.getInventory()?.getItems()?.splice(0, 36)?.forEach((invItems, index) => {
-        if(!invItems) return
+    Player.getInventory()?.getItems()?.forEach((item, slot) => {
+        if (!item?.getNBT()) return;
 
-        res[index] = FileLib.encodeBase64(JSON.stringify({
-            itemLore: invItems.getLore().join("\\"),
-            itemID: invItems.getID()
-        }))
-    })
+        inventory[slot] = NBT.toObject(item.getNBT());
+    });
 
-    return res
+    return inventory;
 }
 
-register("command", (type, ...args) => {
-    if(!type) return chat(`${PREFIX} &cPlease use &6add&c, &6remove &cor &6open`)
-    if(!args?.[0]) return chat(`${PREFIX} &cMissing inventory name`)
+register("command", (command, ...name) => {
+    // Format the command to be lower case incase of a misstype
+    command = command?.toLowerCase();
 
-    const mode = type.toLowerCase()
-    const logsObj = inventoryLogs.find(obj => obj.name === args.join(" "))
+    if (!command) return ChatLib.chat(`
+${PREFIX} §cIncorrect command, try one of the following:
+§7| §6add <name> §7- Add your current inventory to the logs under <name>
+§7| §6remove <name> §7- Remove the inventory with <name> from the logs
+§7| §6open <name> §7- Open logged inventory with <name>
+§7| §6list §7- View available inventroy logs
+    `)
 
-    if(mode === "open") {
-        let invArr = []
-
-        Object.keys(logsObj?.inventory ?? {}).forEach(k => {
-            if(!k) return
-
-            const encObj = JSON.parse(FileLib.decodeBase64(logsObj.inventory[k]))
-            const itemLore = encObj.itemLore
-            const itemID = encObj.itemID
-            const index = parseInt(k) <= 8 ? parseInt(k)+27 : parseInt(k)-9
-
-            invArr[index] = new Item(itemID).setName("").setLore(itemLore.split("\\"))
-        })
-
-        return makeInvChest(args.join(" "), 4).setItems(invArr).open()
-    }
-    
-    if(mode === "remove") {
-        const index = inventoryLogs.findIndex(obj => obj.name === args.join(" "))
-        if(index < 0) return chat(`${PREFIX} &cInventory name not found`)
-
-        inventoryLogs.splice(index, 1)
-
-        saveToFile("data/InventoryLogs.json", inventoryLogs)
-        chat(`${PREFIX} &aSuccessfully removed &c${args.join(" ")} &afrom inventory logs`)
-        return
+    // This doesn't need the inventory name to do operations
+    if (command === "list") {
+        ChatLib.chat(`${PREFIX} §7Current logged inventories:`)
+        Object.keys(inventoryLogs).forEach(inventoryName => ChatLib.chat(`§6- ${inventoryName}`));
+        return;
     }
 
-    if(mode !== "add") return
+    // From here on we require an inventory name so here is a general check
+    // We make use of 0 being falsey in JS here
+    if (!name) return ChatLib.chat(`${PREFIX} §cMissing inventory name`);
 
-    if(!logsObj) inventoryLogs.push({
-        name: args.join(" "),
-        inventory: makeInvObj()
-    })
-    else logsObj.inventory = makeInvObj()
-    
-    saveToFile("data/InventoryLogs.json", inventoryLogs)
-    chat(`${PREFIX} &aSuccessfully added &6${args.join(" ")} &ato inventory logs`)
-}).setName("invlogs")
+    // The name can be spaced so make it one string here
+    name = name.join(" ")
+
+    // Creating an inventory doesn't require us to get the old one
+    if (command === "add") {
+        inventoryLogs[name] = logCurrentInventory();
+        saveToFile("data/InventoryLogs.json", inventoryLogs);
+        ChatLib.chat(`${PREFIX} §aSuccessfully added §6${name} §ato inventory logs`);
+        return;
+    };
+
+    const inventory = inventoryLogs[name];
+    // Check if the inventory exists to open or remove
+    if (!inventory) {
+        return ChatLib.chat(`${PREFIX} §cSorry, §6${name} §cis not a logged inventory`);
+    }
+
+    if (command === "open") {
+        const items = Array(36).fill(null);
+
+        Object.entries(inventory).forEach(([slot, itemNBT]) => {
+            // Calculate the slot to be correct since hotbar is included and is weird
+            if (slot >= 9) { // If the slot is normal inventory
+                slot -= 9;
+            } else { // If the slot is in the hotbar
+                slot += 27;
+            }
+
+            items[slot] = new Item(net.minecraft.item.ItemStack.func_77949_a(NBT.parse(itemNBT).rawNBT)); // #loadItemStackFromNBT
+
+            // Load the lore since it doesn't seem to do it in the function above
+            // Please help this is torture and so fucking scuffed
+            const lore = itemNBT.tag?.display?.Lore;
+            if (lore) {
+                items[slot].setLore(lore);
+            }
+        });
+
+        // Make a fake ChestMenu to show the items in (Possibly in the future remove this dependency)
+        // Return to stop further execution
+        return new ChestMenu(`§8Inventory: §6${name}`, 4)
+            .setItems(items)
+            .open();
+    }
+
+    if (command === "remove") {
+        // This straight up deletes the reference in memory so we save some memory here
+        delete inventoryLogs[name];
+        // Save the deletion
+        saveToFile("data/InventoryLogs.json", inventoryLogs);
+        ChatLib.chat(`${PREFIX} §aSuccessfully removed §6${name} §afrom inventory logs`);
+        return;
+    }
+}).setName("invlogs");
