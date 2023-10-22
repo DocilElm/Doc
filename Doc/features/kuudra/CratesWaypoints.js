c3811901a2e0d668baa13237e8d0cd8d75e63fcb
Doc <65479309+DocilElm@users.noreply.github.com>
2023-10-22 02:31:47 -0700

    Added phase check on CratesWaypoints.js
    Added phase check on CratesWaypoints.js
    now it stops displaying the waypoints once p1 is done in kuudra

--- Doc/features/kuudra/CratesWaypoints.js
+++ Doc/features/kuudra/CratesWaypoints.js
@@ -1,10 +1,14 @@
 import renderBeaconBeam from "../../../BeaconBeam"
 import { addEvent } from "../../FeatureBase"
+import { onChatPacket } from "../../classes/Events"
 import { EntityGiantZombie } from "../../utils/Utils"
 
 let crates = []
+let toggle = true
 
 addEvent("cratesWaypoints", "Kuudra", register("step", () => {
+    if(!World.isLoaded() || !toggle) return
+
     crates = World.getAllEntitiesOfType(EntityGiantZombie).filter(a => a.getY() < 67).map(entity => {
         const entityYaw = entity.getYaw()
         const x = entity.getX() + 5 * Math.cos((entityYaw + 130) * (Math.PI / 180))
@@ -13,13 +17,20 @@ addEvent("cratesWaypoints", "Kuudra", register("step", () => {
         return [x, z]
     })
 }).setFps(5), null, [
+    onChatPacket(() => {
+        toggle = false
+    }).setCriteria(/^\[NPC\] Elle\: OMG\! Great work collecting my supplies\!$/),
+
     register("renderWorld", () => {
-        if(!World.isLoaded() || !crates) return
+        if(!World.isLoaded() || !crates || !toggle) return
     
         crates.forEach(entity => {
-            renderBeaconBeam(entity[0], 75, entity[1], 50, 205, 50, 1, true)
+            renderBeaconBeam(entity[0], 75, entity[1], 255/255, 0/255, 255/255, 1, true)
         })
     })
 ], "Kuudra")
 
-register("worldUnload", () => crates = [])
+register("worldUnload", () => {
+    crates = []
+    toggle = true
+})
