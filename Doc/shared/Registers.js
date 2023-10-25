951359cfa2925645b1ca2b601a867f60b31bfdfa
Dalwyn <100111505+DalwynWasTaken@users.noreply.github.com>
2023-10-25 14:34:01 +0200

    Add onChatPacket event
    Add the on chat packet event

--- Doc/shared/Registers.js
+++ Doc/shared/Registers.js
@@ -12,6 +12,34 @@ FeatureManager
     .createCustomEvent("step", (fn, stepAmount) => 
         register("step", fn).setFps(stepAmount).unregister()
     )
+    .createCustomEvent("onChatPacket", (fn, criteria) => {
+        register("packetReceived", (packet, event) => {
+            // Check if the packet is for the actionbar
+            if (packet.func_148916_d()) return
+
+            const chatComponent = packet.func_148915_c()        
+            const formatted = chatComponent?.func_150254_d()
+            const unformatted = formatted?.removeFormatting()
+        
+            if (!unformatted) return
+            
+            // Check if the criteria is a regex or a string
+            // Regex is way more intensive so only use that if needed
+            if (typeof(criteria) === "string") {
+                if (unformatted !== criteria) return
+
+                fn(unformatted, event, formatted)
+            } else if (criteria instanceof RegExp) {
+                const match = unformatted.match(criteria)
+                if (!match) return
+                
+                // Call the eventFunction with parameters being the
+                // matched groups as different params, ending with
+                // the packet even and formatted message
+                fn(...match.slice(1), event, formatted)
+            }
+        }).setFilteredClass(net.minecraft.network.play.server.S02PacketChat)
+    })
 
 /*
 export const CustomEvents = {
