54a9b4e22be9791f0d028b5e1f6fb7869bc79a75
Doc <65479309+DocilElm@users.noreply.github.com>
2023-10-30 03:40:01 -0700

    Fix FatalTempoDisplay
    Fix FatalTempoDisplay
    fixed it having the old rendering code

--- Doc/features/kuudra/FatalTempoDisplay.js
+++ Doc/features/kuudra/FatalTempoDisplay.js
@@ -14,42 +14,59 @@ const requiredWorld = "Kuudra"
 let hitsAmount = 0
 let lastHit = null
 let ftLvl = 0
+let stringToDraw = null
 
 // Default display
-editGui.onRender(() => editGui.renderString(`&cFatal Tempo&f: &63s &7(Hits: 0 0%)`))
+editGui.onRender(() => {
+    Renderer.translate(editGui.getX(), editGui.getY())
+    Renderer.scale(editGui.getScale())
+    Renderer.drawStringWithShadow(`&cFatal Tempo&f: &63s &7(Hits: 0 0%)`, 0, 0)
+    Renderer.finishDraw()
+})
 
 // Logic
 const registerWhen = () => WorldState.getCurrentWorld() === requiredWorld && config.ftDisplay
-
 const getFtPercent = () => (hitsAmount * ftLvl) * 10
+
 const soundHandler = () => {
     const heldItem = Player.getHeldItem()
-    if(!heldItem || getSkyblockId(heldItem) !== "TERMINATOR" || !getExtraAttribute(heldItem, "ultimate_fatal_tempo")) return
+    if (!heldItem || getSkyblockId(heldItem) !== "TERMINATOR" || !getExtraAttribute(heldItem, "ultimate_fatal_tempo")) return
 
     ftLvl = getExtraAttribute(heldItem, "ultimate_fatal_tempo")
     hitsAmount++
     lastHit = Date.now()
 }
 
-// Events
-new Event(feature, "soundPlay", soundHandler, registerWhen, "random.successful_hit")
-new Event(feature, "soundPlay", soundHandler, registerWhen, "tile.piston.out")
-
-new Event(feature, "renderOverlay", () => {
-    if(!hitsAmount || !lastHit) return
-    if(Date.now()-lastHit >= 3000) return hitsAmount = 0, ftLvl = 0, lastHit = null
+const makeStringToDraw = () => {
+    if (!hitsAmount || !lastHit) return stringToDraw = null
+    if (Date.now()-lastHit >= 3000) return hitsAmount = 0, ftLvl = 0, lastHit = null
 
     const ftTime = ((3000-(Date.now()-lastHit))/1000).toFixed(2)
     const ftPercent = getFtPercent() >= 200 ? 200 : getFtPercent()
 
-    editGui.renderString(`&cFatal Tempo&f: &6${ftTime}s &7(Hits: ${mathTrunc(hitsAmount)} ${ftPercent}%)`)
-}, registerWhen)
+    stringToDraw = `&cFatal Tempo&f: &6${ftTime}s &7(Hits: ${mathTrunc(hitsAmount)} ${ftPercent}%)`
+}
+
+const renderFatalTempo = () => {
+    if (!stringToDraw) return
+
+    Renderer.translate(editGui.getX(), editGui.getY())
+    Renderer.scale(editGui.getScale())
+    Renderer.drawStringWithShadow(stringToDraw, 0, 0)
+    Renderer.finishDraw()
+}
+
+// Events
+new Event(feature, "soundPlay", soundHandler, registerWhen, "random.successful_hit")
+new Event(feature, "soundPlay", soundHandler, registerWhen, "tile.piston.out")
+new Event(feature, "tick", makeStringToDraw, registerWhen)
+new Event(feature, "renderOverlay", renderFatalTempo, () => WorldState.getCurrentWorld() === requiredWorld && config.ftDisplay && stringToDraw)
 
 new Event(feature, "worldUnload", () => {
     hitsAmount = 0
     ftLvl = 0
     lastHit = null
-}, null)
+})
 
 // Starting events
 feature.start()
