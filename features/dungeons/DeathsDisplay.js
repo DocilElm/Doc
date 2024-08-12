95d22f1222122e64ecb0bfa3ccb990bd23896ada
Doc <65479309+DocilElm@users.noreply.github.com>
2024-08-11 20:18:00 -0700

    feat: add DeathsDisplay

--- features/dungeons/DeathsDisplay.js
+++ features/dungeons/DeathsDisplay.js
@@ -3,25 +3,25 @@ import { Event } from "../../core/Event"
 import Feature from "../../core/Feature"
 import DraggableGui from "../../shared/DraggableGui"
 
-const editGui = new DraggableGui("cryptsDisplay").setCommandName("editcryptsDisplay")
+const editGui = new DraggableGui("deathsDisplay").setCommandName("editdeathsDisplay")
 
 editGui.onDraw(() => {
     Renderer.translate(editGui.getX(), editGui.getY())
     Renderer.scale(editGui.getScale())
-    Renderer.drawStringWithShadow("&aCrypts&f: &65", 0, 0)
+    Renderer.drawStringWithShadow("&8&lDeaths&f: &43", 0, 0)
     Renderer.finishDraw()
 })
 
-new Feature("cryptsDisplay", "catacombs")
+new Feature("deathsDisplay", "catacombs")
     .addEvent(
         new Event("renderOverlay", () => {
             if (editGui.isOpen()) return
 
-            const crypts = Dungeons.getCryptsAmount()
+            const amount = Dungeons.getTeamDeaths()
 
             Renderer.translate(editGui.getX(), editGui.getY())
             Renderer.scale(editGui.getScale())
-            Renderer.drawStringWithShadow(`&aCrypts&f: ${crypts >= 5 ? "&6" : "&c"}${crypts}`, 0, 0)
+            Renderer.drawStringWithShadow(`&8&lDeaths&f: ${amount >= 3 ? "&4" : "&c"}${amount}`, 0, 0)
             Renderer.finishDraw()
         })
     )
