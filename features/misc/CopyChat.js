989a1d19ad59873afdd1e584a59a5244322933c0
Doc <65479309+DocilElm@users.noreply.github.com>
2024-08-12 21:02:47 -0700

    feat: add CopyChat

---
+++ features/misc/CopyChat.js
@@ -0,0 +1,47 @@
+import { Event } from "../../core/Event"
+import Feature from "../../core/Feature"
+import { TextHelper } from "../../shared/TextHelper"
+
+const Mouse = org.lwjgl.input.Mouse
+
+const add = (comp, arr, isBelow) => {
+    if (!comp || isBelow && !comp.func_150261_e()?.removeFormatting().startsWith(" ")) return
+    const isCtrlDown = Client.isControlDown()
+
+    const msg = isCtrlDown
+        ? comp.func_150261_e()?.replace(/§/g, "&")
+        : comp.func_150261_e()?.removeFormatting()?.replace(/§z/g, "")
+    if (arr.indexOf(msg) !== -1) return
+
+    arr.push(msg)
+}
+
+new Feature("copyChat")
+    .addEvent(
+        new Event("guiMouseClick", (_, __, mouseButton) => {
+            if (!Client.isInChat() || mouseButton !== 1) return
+
+            const y = Mouse.getY()
+            const chatGui = Client.getChatGUI()
+            const scale = Client.settings.getSettings().field_74335_Z
+            const chatWidth = 600 * scale
+
+            let normal = []
+            let below = []
+            let below2 = []
+
+            for (let idx = 0; idx < chatWidth; idx += 5) {
+                let comp = chatGui.func_146236_a(idx, y)
+                let compbelow = chatGui.func_146236_a(idx, y - (10 * scale))
+                // Just in case it's a triple line message
+                let compbelow2 = chatGui.func_146236_a(idx, y - (20 * scale))
+
+                add(comp, normal)
+                add(compbelow, below, true)
+                add(compbelow2, below2, true)
+            }
+
+            ChatLib.command(`ct copy ${[...normal, ...below, ...below2].join("\n")}`, true)
+            ChatLib.chat(`${TextHelper.PREFIX} &aCopied message to clipboard`)
+        })
+    )
