2538d8e226c8df45811985ef0f573379fb2051c5
Doc <65479309+DocilElm@users.noreply.github.com>
2024-09-10 15:13:14 -0700

    feat: fix NoCursorReset disabling other mods feature

--- features/misc/NoCursorReset.js
+++ features/misc/NoCursorReset.js
@@ -2,9 +2,12 @@ import config from "../../config"
 import { Event } from "../../core/Event"
 import EventEnums from "../../core/EventEnums"
 import Feature from "../../core/Feature"
+import { TextHelper } from "../../shared/TextHelper"
 
 // Credits: https://github.com/Skytils/SkytilsMod/blob/1.x/src/main/kotlin/gg/skytils/skytilsmod/mixins/hooks/util/MouseHelperHook.kt
 
+const blacklistedMods = ["skytils", "skyblockaddons"]
+const hasBlacklisted = blacklistedMods.some(it => net.minecraftforge.fml.common.Loader.isModLoaded(it))
 const Mouse = Java.type("org.lwjgl.input.Mouse")
 const GLDisplay = Java.type("org.lwjgl.opengl.Display")
 
@@ -12,30 +15,38 @@ const GLDisplay = Java.type("org.lwjgl.opengl.Display")
 let originalMouseHelper = Client.getMinecraft().field_71417_B // mouseHelper
 let windowClosed = null
 
-// Set the mc instance's [mouseHelper] to a custom made one
-// which will have a check before [ungrabMouseCursor] calls [setCursorPosition]
-Client.getMinecraft().field_71417_B = new JavaAdapter(net.minecraft.util.MouseHelper, {
-    // ungrabMouseCursor()
-    func_74373_b() {
-        // If no cursor reset is set to false
-        // we implement the normal behavior
-        if (!config().noCursorReset) {
-            Mouse.setCursorPosition(GLDisplay.getWidth() / 2, GLDisplay.getHeight() / 2)
+if (!hasBlacklisted) {
+    // Set the mc instance's [mouseHelper] to a custom made one
+    // which will have a check before [ungrabMouseCursor] calls [setCursorPosition]
+    Client.getMinecraft().field_71417_B = new JavaAdapter(net.minecraft.util.MouseHelper, {
+        // ungrabMouseCursor()
+        func_74373_b() {
+            // If no cursor reset is set to false
+            // we implement the normal behavior
+            if (!config().noCursorReset) {
+                Mouse.setCursorPosition(GLDisplay.getWidth() / 2, GLDisplay.getHeight() / 2)
+                Mouse.setGrabbed(false)
+
+                return
+            }
+
+            // Check whether we should re-set the position of the cursor or not
+            // by checking if the time between [windowClosed] and [now] (whenever this method is called)
+            // is below 100ms
+            if (!Client.isInGui() || !windowClosed || (Date.now() - windowClosed) > 100) {
+                Mouse.setCursorPosition(GLDisplay.getWidth() / 2, GLDisplay.getHeight() / 2)
+                windowClosed = null
+            }
+
             Mouse.setGrabbed(false)
-
-            return
         }
+    })
+}
 
-        // Check whether we should re-set the position of the cursor or not
-        // by checking if the time between [windowClosed] and [now] (whenever this method is called)
-        // is below 100ms
-        if (!Client.isInGui() || !windowClosed || (Date.now() - windowClosed) > 100) {
-            Mouse.setCursorPosition(GLDisplay.getWidth() / 2, GLDisplay.getHeight() / 2)
-            windowClosed = null
-        }
+config().getConfig().registerListener("noCursorReset", (_, v) => {
+    if (!v || !hasBlacklisted) return
 
-        Mouse.setGrabbed(false)
-    }
+    ChatLib.chat(`${TextHelper.PREFIX} &cFeature &7[&6NoCursorReset&7]&c was enabled while having blacklisted mods meaning this feature will not work because you have either &6Skytils &cor &6SkyblockAddons&r`)
 })
 
 new Feature("noCursorReset")
@@ -46,6 +57,8 @@ new Feature("noCursorReset")
     )
 
 register("gameUnload", () => {
+    if (hasBlacklisted) return
+
     // field_71417_B - mouseHelper
     Client.getMinecraft().field_71417_B = originalMouseHelper
     windowClosed = null
