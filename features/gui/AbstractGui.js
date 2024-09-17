ae33a7c684ad7883b390a6ec25a7cc760e82536b
Doc <65479309+DocilElm@users.noreply.github.com>
2024-09-17 14:16:45 -0700

    internal: AbstractGui remove `Save` button & make scheme shared

--- features/gui/AbstractGui.js
+++ features/gui/AbstractGui.js
@@ -2,6 +2,12 @@ import ElementUtils from "../../../DocGuiLib/core/Element"
 import { CenterConstraint, CramSiblingConstraint, OutlineEffect, ScrollComponent, UIBlock, UIText } from "../../../Elementa"
 import { Button } from "./Button"
 
+export const textInputScheme = {
+    TextInput: {
+        background: { color: [45, 58, 75, 80] }
+    }
+}
+
 export class AbstractGui {
     constructor(title) {
         this.title = title.addColor()
@@ -19,37 +25,20 @@ export class AbstractGui {
 
         this.scrollComp = new ScrollComponent()
             .setX(new CenterConstraint())
-            .setY((25).pixels())
+            .setY(new CramSiblingConstraint(5))
             .setWidth((100).percent())
             .setHeight((65).percent())
-            .setChildOf(this.bgBoxComp)
-
-        this.centeredBottom = new UIBlock(ElementUtils.getJavaColor([0, 0, 0, 0]))
-            .setX(new CenterConstraint())
-            .setY((80).percent())
-            .setWidth((40).percent())
-            .setHeight((10).percent())
             .setChildOf(this.bgBoxComp)
 
         this.addButton = new Button("&aAdd", [45, 58, 75, 255], [35, 196, 3, 200])
         this.addButton.onClick(() => this.onAdd())
         this.addButton
             .component
-            .setX(new CramSiblingConstraint(3))
-            .setY((0).percent())
-            .setWidth((45).percent())
-            .setHeight((80).percent())
-            .setChildOf(this.centeredBottom)
-
-        this.saveButton = new Button("&bSave", [45, 58, 75, 255], [4, 103, 132, 255])
-        this.saveButton.onClick(() => this.onSave())
-        this.saveButton
-            .component
-            .setX(new CramSiblingConstraint(3))
-            .setY((0).percent())
-            .setWidth((45).percent())
-            .setHeight((80).percent())
-            .setChildOf(this.centeredBottom)
+            .setX(new CenterConstraint())
+            .setY((80).percent())
+            .setWidth((40).percent())
+            .setHeight((10).percent())
+            .setChildOf(this.bgBoxComp)
     }
 
     /**
