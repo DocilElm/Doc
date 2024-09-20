40e54a288f664afe9c81c28db448e5a1113d8b46
Doc <65479309+DocilElm@users.noreply.github.com>
2024-09-20 14:48:23 -0700

    internal: AbstractGui add `bottomLineEffect` & columns support

--- features/gui/AbstractGui.js
+++ features/gui/AbstractGui.js
@@ -1,5 +1,5 @@
 import ElementUtils from "../../../DocGuiLib/core/Element"
-import { CenterConstraint, CramSiblingConstraint, OutlineEffect, ScrollComponent, UIBlock, UIText } from "../../../Elementa"
+import { CenterConstraint, OutlineEffect, ScrollComponent, UIBlock, UIText } from "../../../Elementa"
 import { Button } from "./Button"
 
 export const textInputScheme = {
@@ -11,10 +11,59 @@ export const textInputScheme = {
     }
 }
 
+const Effect = Java.type("gg.essential.elementa.effects.Effect")
+
+/** 
+ * @param {number[]} color [0 - 255]
+ * @param {number} thickness The thickness of the line
+ * @param {UIComponent[]} comps The components this effect should use to calculate the width of the line
+ * 
+*/
+export const bottomLineEffect = (color, thickness = 1, comps = [], shouldPad = false) => {
+    return new JavaAdapter(Effect, {
+        getData() {
+            const res = [null, 0]
+            if (comps.length === 1) {
+                res[0] = comps[0].getLeft()
+                res[1] = comps[0].getWidth()
+                return res
+            }
+
+            let pad = (comps[1].getLeft() - comps[0].getRight()) / 2 + 0.25
+
+            for (let idx = 0; idx < comps.length; idx++) {
+                let comp = comps[idx]
+                if (idx === 0) res[0] = comp.getLeft()
+                res[1] += comp.getWidth() + (shouldPad ? pad : 0)
+            }
+
+            return res
+        },
+        beforeChildrenDraw() {
+            const bounds = this.boundComponent
+            const [ y, height ] = [ bounds.getTop(), bounds.getHeight() ]
+
+            let [ startX, totalWidth ] = this.getData()
+
+            Renderer.drawLine(
+                Renderer.color(...color),
+                startX,
+                y + height + thickness,
+                startX + totalWidth,
+                y + height + thickness,
+                thickness
+            )
+        }
+    })
+}
+
 export class AbstractGui {
-    constructor(name) {
+    constructor(name, columns, columnData = { startX: 5, padding: 28 }) {
         this.list = []
+        this.columns = columns
+        this.columnData = columnData
         this.title = `&b&lDoc ${name}`.addColor()
+
         this.bgBoxComp = new UIBlock(ElementUtils.getJavaColor([3, 7, 17, 255]))
             .setX(new CenterConstraint())
             .setY(new CenterConstraint())
@@ -27,11 +76,15 @@ export class AbstractGui {
             .setY((5).percent())
             .setChildOf(this.bgBoxComp)
 
+        if (this.columns) {
+            this._makeColumns()
+        }
+
         this.scrollComp = new ScrollComponent()
             .setX(new CenterConstraint())
-            .setY(new CramSiblingConstraint(5))
+            .setY((15).percent())
             .setWidth((100).percent())
-            .setHeight((65).percent())
+            .setHeight((60).percent())
             .setChildOf(this.bgBoxComp)
 
         this.addButton = new Button("&aAdd", [45, 58, 75, 255], [35, 196, 3, 200])
@@ -43,6 +96,25 @@ export class AbstractGui {
             .setWidth((40).percent())
             .setHeight((10).percent())
             .setChildOf(this.bgBoxComp)
+    }
+
+    _makeColumns() {
+        if (this.columns.length === 1) {
+            new UIText(this.columns[0])
+                .setX(new CenterConstraint())
+                .setY((10).percent())
+                .setChildOf(this.bgBoxComp)
+            return
+        }
+
+        for (let idx = 0; idx < this.columns.length; idx++) {
+            let text = this.columns[idx]
+            let x = this.columnData.startX + (this.columnData.padding * idx)
+            new UIText(text)
+                .setX((x).percent())
+                .setY((10).percent())
+                .setChildOf(this.bgBoxComp)
+        }
     }
 
     /**
