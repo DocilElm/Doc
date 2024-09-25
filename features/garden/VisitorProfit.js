810ef8f10aeff653f247df4340c20d3675e50e5c
Doc <65479309+DocilElm@users.noreply.github.com>
2024-09-25 16:40:15 -0700

    feat: changed VisitorBzButton to GardenEvents

--- features/garden/VisitorProfit.js
+++ features/garden/VisitorProfit.js
@@ -4,19 +4,16 @@ import { Event } from "../../core/Event"
 import EventEnums from "../../core/EventEnums"
 import Feature from "../../core/Feature"
 import DraggableGui from "../../shared/DraggableGui"
-import { GardenApi } from "../../shared/Persistence"
 import { TextHelper } from "../../shared/TextHelper"
+import { onVisitor } from "./GardenEvents"
 
 const editGui = new DraggableGui("visitorProfit").setCommandName("editvisitorProfit")
-const visitorSet = new Set(Object.keys(GardenApi.Visitors))
 const visitorsList = new Map()
 // Fun regex
 const requiredItemsRegex = /^ ([A-z ]+)([\d,]+)?$/
 const copperRegex = /^ \+([\d,]+) Copper$/
 const rareItemregex = /^ (?:◆)?([\w ]+)$/
 const visitorDialogRegex = /^\[NPC\] ([\w\. ]+): (.+)$/
-
-let inVisitor = false
 
 editGui.onDraw(() => {
     Renderer.translate(editGui.getX(), editGui.getY())
@@ -26,101 +23,6 @@ editGui.onDraw(() => {
 })
 
 const feat = new Feature("visitorProfitDisplay", "garden")
-    .addEvent(
-        new Event(EventEnums.PACKET.SERVER.WINDOWOPEN, (title) => {
-            inVisitor = visitorSet.has(title)
-            feat.update()
-        })
-    )
-    .addSubEvent(
-        new Event(EventEnums.PACKET.CUSTOM.WINDOWCLOSE, () => {
-            inVisitor = false
-            feat.update()
-        }),
-        () => inVisitor
-    )
-    .addSubEvent(
-        new Event(EventEnums.PACKET.SERVER.WINDOWITEMS, (items) => {
-            // Just in case this happens some day
-            if (!items[13] || !items[29]) return
-
-            const visitorSkull = new Item(items[13])
-            const isVisitor = /^Offers Accepted: [\d]+$/.test(visitorSkull.getLore()?.[4]?.removeFormatting())
-            if (!isVisitor) {
-                inVisitor = false
-                feat.update()
-
-                return
-            }
-
-            const visitorName = visitorSkull.getName().removeFormatting()
-            const currentData = {
-                name: visitorSkull.getName(),
-                requiredItems: [],
-                copperAmount: 0,
-                totalPrice: 0,
-                profit: 0,
-                rareItem: null
-            }
-
-            const acceptButton = new Item(items[29])
-            const acceptLore = acceptButton.getLore()
-
-            for (let idx = 2; idx < acceptLore.length; idx++) {
-                let lore = acceptLore[idx].removeFormatting()
-
-                let requiredItemsMatch = lore.match(requiredItemsRegex)
-                if (idx < 5 && requiredItemsMatch) {
-                    let requiredName = requiredItemsMatch[1].replace(/ x/, "").replace(/ /g, "_").toUpperCase()
-                    let amount = Math.floor(requiredItemsMatch[2]) || 1
-
-                    let price = Price.getSellPrice(AtomxApi.getGardenItemID()[requiredName])
-                    let totalPrice = price * amount
-
-                    currentData.requiredItems.push(acceptLore[idx])
-                    currentData.totalPrice += (totalPrice || 0)
-
-                    continue
-                }
-
-                let copperMatch = lore.match(copperRegex)
-                if (copperMatch) {
-                    currentData.copperAmount = Math.floor(copperMatch[1])
-
-                    continue
-                }
-
-                let rareItemMatch = lore.match(rareItemregex)
-                if (rareItemMatch && idx > 5) {
-                    let rareItemName = rareItemMatch[1].replace(/^ /, "")
-                    let rareName = AtomxApi.getGardenRareItems()[rareItemName]
-
-                    if (!rareName) continue
-
-                    currentData.rareItem = rareName
-                    ChatLib.chat(`${TextHelper.PREFIX} &aVisitor &b${visitorName}&a had &b${rareItemName}`)
-
-                    continue
-                }
-            }
-
-            const totalItemPrice = currentData.totalPrice
-            const totalCopperPrice = (Price.getSellPrice("ENCHANTMENT_GREEN_THUMB_1") / 1500) * currentData.copperAmount
-            currentData.profit = (totalCopperPrice - totalItemPrice)
-
-            if (currentData.rareItem) {
-                const rareItemPrice = Price.getSellPrice(currentData.rareItem)
-                const currentProfit = currentData.profit <= 0 ? -currentData.profit : +currentData.profit
-
-                currentData.profit = (rareItemPrice - currentProfit)
-            }
-
-            visitorsList.set(visitorName, currentData)
-            inVisitor = false
-            feat.update()
-        }),
-        () => inVisitor
-    )
     .addSubEvent(
         new Event("renderOverlay", () => {
             if (editGui.isOpen()) return
@@ -172,5 +74,68 @@ const feat = new Feature("visitorProfitDisplay", "garden")
     )
     .onUnregister(() => {
         visitorsList.clear()
-        inVisitor = false
     })
+
+onVisitor((visitorName, acceptButton) => {
+    const currentData = {
+        name: visitorName,
+        requiredItems: [],
+        copperAmount: 0,
+        totalPrice: 0,
+        profit: 0,
+        rareItem: null
+    }
+    const acceptLore = acceptButton.getLore()
+
+    for (let idx = 2; idx < acceptLore.length; idx++) {
+        let lore = acceptLore[idx].removeFormatting()
+
+        let requiredItemsMatch = lore.match(requiredItemsRegex)
+        if (idx < 5 && requiredItemsMatch) {
+            let requiredName = requiredItemsMatch[1].replace(/ x/, "").replace(/ /g, "_").toUpperCase()
+            let amount = Math.floor(requiredItemsMatch[2]) || 1
+
+            let price = Price.getSellPrice(AtomxApi.getGardenItemID()[requiredName])
+            let totalPrice = price * amount
+
+            currentData.requiredItems.push(acceptLore[idx])
+            currentData.totalPrice += (totalPrice || 0)
+
+            continue
+        }
+
+        let copperMatch = lore.match(copperRegex)
+        if (copperMatch) {
+            currentData.copperAmount = Math.floor(copperMatch[1])
+
+            continue
+        }
+
+        let rareItemMatch = lore.match(rareItemregex)
+        if (rareItemMatch && idx > 5) {
+            let rareItemName = rareItemMatch[1].replace(/^ /, "")
+            let rareName = AtomxApi.getGardenRareItems()[rareItemName]
+
+            if (!rareName) continue
+
+            currentData.rareItem = rareName
+            ChatLib.chat(`${TextHelper.PREFIX} &aVisitor &b${visitorName.removeFormatting()}&a has &b${rareItemName}`)
+
+            continue
+        }
+    }
+
+    const totalItemPrice = currentData.totalPrice
+    const totalCopperPrice = (Price.getSellPrice("ENCHANTMENT_GREEN_THUMB_1") / 1500) * currentData.copperAmount
+    currentData.profit = (totalCopperPrice - totalItemPrice)
+
+    if (currentData.rareItem) {
+        const rareItemPrice = Price.getSellPrice(currentData.rareItem)
+        const currentProfit = currentData.profit <= 0 ? -currentData.profit : +currentData.profit
+
+        currentData.profit = (rareItemPrice - currentProfit)
+    }
+
+    visitorsList.set(visitorName.removeFormatting(), currentData)
+    feat.update()
+})
