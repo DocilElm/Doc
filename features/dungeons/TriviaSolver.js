e1dcf56a8865bdf2e38faebe9254d19c2a41a55a
Doc <65479309+DocilElm@users.noreply.github.com>
2025-02-13 15:04:21 -0700

    TriviaSolver: highlight correct answer's block

--- features/dungeons/TriviaSolver.js
+++ features/dungeons/TriviaSolver.js
@@ -1,7 +1,11 @@
+import config from "../../config"
 import { Event } from "../../core/Event"
 import EventEnums from "../../core/EventEnums"
 import Feature from "../../core/Feature"
 import { Persistence } from "../../shared/Persistence"
+import { onPuzzleRotationExit, onPuzzleScheduledRotation } from "../../shared/PuzzleRoomScanner"
+import { RenderHelper } from "../../shared/Render"
+import { TextHelper } from "../../shared/TextHelper"
 
 const triviaAnswers = Persistence.getDataFromFileOrLink("TriviaAnswers.json", "https://raw.githubusercontent.com/DocilElm/Doc-Data/refs/heads/main/dungeons/TriviaSolutions.json")
 const Blocks = net.minecraft.init.Blocks
@@ -23,6 +27,9 @@ Object.keys(triviaAnswers)?.forEach(key => {
     solutions.set(key, value)
 })
 
+let enteredRoom = null
+let currentBlock = null
+let currentRotation = null
 let currentQuestion = null
 let currentSymbol = null
 let chat = []
@@ -35,13 +42,9 @@ const reset = () => {
     chat = []
     currentSolutions = []
     answers = []
-    // enteredRoom = null
-    // currentQuestion = null
-    // currSolutions = []
-    // chat = []
-    // answers = []
-    // currentBlock = null
-    // currentRotation = null
+    enteredRoom = null
+    currentBlock = null
+    currentRotation = null
 }
 
 /** @param {string} msg */
@@ -77,6 +80,9 @@ const sendChat = () => {
 const feat = new Feature("triviaQuizSolver", "catacombs")
     .addEvent(
         new Event(EventEnums.PACKET.SERVER.CHAT, (unformatted, event) => {
+            if (/^\[STATUE\] Oruo the Omniscient\: .+$/.test(unformatted) && !enteredRoom)
+                return enteredRoom = Date.now()
+
             const match = unformatted.match(/^ +(.+\?)$/)
             if (match) {
                 cancel(event)
@@ -96,6 +102,7 @@ const feat = new Feature("triviaQuizSolver", "catacombs")
                 if (isSolution) {
                     currentSolutions.push(answerMatch[2])
                     currentSymbol = answerMatch[1]
+                    feat.update()
                 }
 
                 const toDisplay = isSolution
@@ -122,11 +129,67 @@ const feat = new Feature("triviaQuizSolver", "catacombs")
                 /^\[STATUE\] Oruo the Omniscient\: I bestow upon you all the power of a hundred years\!$/.test(unformatted) ||
                 /^\[STATUE\] Oruo the Omniscient\: Yikes/.test(unformatted)
             ) {
-                // TODO: post time taken
+                ChatLib.chat(`${TextHelper.PREFIX} &aTrivia took&f: &6${((Date.now() - enteredRoom) / 1000).toFixed(2)}s`)
                 reset()
             }
         })
     )
+    .addSubEvent(
+        new Event(EventEnums.STEP, () => {
+            World.getAllEntitiesOfType(net.minecraft.entity.item.EntityArmorStand)
+                .forEach(entity => {
+                    const match = entity.getName()?.removeFormatting()?.match(/([ⓐⓑⓒ]) ([^.]+)[.+]?/)
+                    if (!match) return
+
+                    let [ _, question, answer ] = match
+                    if (currentSolutions.some(it => it === answer))
+                        return entity.entity.func_96094_a(`§6${question} §a§l${answer}`)
+
+                    entity.entity.func_96094_a(`§6${question} §4${answer}`)
+                })
+        }, 1),
+        () => currentRotation !== null
+    )
+    .addSubEvent(
+        new Event("tick", () => {
+            currentBlock = World.getBlockAt(...TextHelper.getRealCoord(relativeCoords[currentSymbol], currentRotation))
+            currentSymbol = null
+            feat.update()
+        }),
+        () => currentRotation !== null && currentSymbol
+    )
+    .addSubEvent(
+        new Event(EventEnums.PACKET.SERVER.CHAT, () => {
+            currentBlock = null
+            currentSymbol = null
+        }, /^\[STATUE\] Oruo the Omniscient\: [\w]+ answered Question #\d correctly\!$/),
+        () => currentBlock
+    )
+    .addSubEvent(
+        new Event("renderWorld", () => {
+            RenderHelper.filledBlock(currentBlock, 0, 255, 0, 80, false)
+        }),
+        () => currentBlock
+    )
     .onUnregister(() => {
         reset()
     })
+
+onPuzzleScheduledRotation((rotation) => {
+    if (currentRotation != null || !config().triviaQuizSolver) return
+
+    const glowstoneBlock = World.getBlockAt(...TextHelper.getRealCoord(relativeCoords.glowstone, rotation)).type.mcBlock
+    const goldBlock = World.getBlockAt(...TextHelper.getRealCoord(relativeCoords.gold, rotation)).type.mcBlock
+
+    if (glowstoneBlock !== BlockGlowstone || goldBlock !== BlockGold) return
+
+    currentRotation = rotation
+    feat.update()
+})
+
+onPuzzleRotationExit(() => {
+    if (currentRotation == null) return
+
+    reset()
+    feat.update()
+})
