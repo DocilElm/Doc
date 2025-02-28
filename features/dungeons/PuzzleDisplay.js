324b1ce49232a65224feecdd93317bbe84d73c78
Doc <65479309+DocilElm@users.noreply.github.com>
2025-02-28 04:37:58 -0700

    feat: fix `PuzzleDisplay` not displaying puzzle count properly

--- features/dungeons/PuzzleDisplay.js
+++ features/dungeons/PuzzleDisplay.js
@@ -1,6 +1,8 @@
+import AtomxApi from "../../../Atomx/AtomxApi"
 import Dungeons from "../../../Atomx/skyblock/Dungeons"
 import config from "../../config"
 import { Event } from "../../core/Event"
+import EventEnums from "../../core/EventEnums"
 import Feature from "../../core/Feature"
 import DraggableGui from "../../shared/DraggableGui"
 
@@ -10,8 +12,10 @@ const PuzzleEnums = {
     1: "&a✔",
     2: "&c✖"
 }
+const puzzleRegex = AtomxApi.getRegexData()?.Dungeons?.PuzzlesAmount
 
 let puzzles = {}
+let puzzleCount = 0
 
 editGui.onDraw(() => {
     Renderer.translate(editGui.getX(), editGui.getY())
@@ -20,20 +24,28 @@ editGui.onDraw(() => {
     Renderer.finishDraw()
 })
 
-new Feature("puzzlesDisplay", "catacombs")
+const feat = new Feature("puzzlesDisplay", "catacombs")
     .addEvent(
+        new Event(EventEnums.PACKET.SERVER.TABADD, (amount) => {
+            puzzleCount = +amount
+            feat.update()
+        }, puzzleRegex)
+    )
+    .addSubEvent(
         new Event("renderOverlay", () => {
             if (editGui.isOpen()) return
 
-            const amount = Dungeons.getPuzzlesAmount()
-
             Renderer.translate(editGui.getX(), editGui.getY())
             Renderer.scale(editGui.getScale())
-            Renderer.drawStringWithShadow(`&d&lPuzzles&f: ${amount >= 4 ? "&6" : "&a"}${amount}\n${Object.values(puzzles).join("\n")}`, 0, 0)
+            Renderer.drawStringWithShadow(`&d&lPuzzles&f: ${puzzleCount >= 4 ? "&6" : "&a"}${puzzleCount}\n${Object.values(puzzles).join("\n")}`, 0, 0)
             Renderer.finishDraw()
-        })
+        }),
+        () => puzzleCount !== null
     )
-    .onUnregister(() => puzzles = {})
+    .onUnregister(() => {
+        puzzles = {}
+        puzzleCount = 0
+    })
 
 Dungeons.onPuzzleEvent((puzzleName, event, failedBy) => {
     if (!config().puzzlesDisplay) return
